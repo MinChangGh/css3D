6cd44de26bc08fa692fc3f5c52e1022dc3d66be8
MinChangGh <799022552@qq.com>
2019-05-06 17:43:52 +0800

    fs

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+
+<div>456</div>
+document.getElementById('root')
+)
